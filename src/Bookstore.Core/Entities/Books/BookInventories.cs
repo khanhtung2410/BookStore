52e51cf3a7b6eba50e2691f337deeffa6c878b25
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2025-10-15 15:04:34 +0700

    move location

---
+++ src/Bookstore.Core/Entities/Books/BookInventories.cs
@@ -0,0 +1,50 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Entities.Books
+{
+    [Table("BookInventories")]
+    public class BookInventory : FullAuditedEntity<int>, IValidatableObject
+    {
+        [ForeignKey(nameof(BookEdition))]
+        public int BookEditionId { get; set; }
+        public virtual BookEdition BookEdition { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal BuyPrice { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal SellPrice { get; set; }
+
+        [Required]
+        public long StockQuantity { get; set; }
+
+        public BookInventory() { }
+
+        public BookInventory(int bookEditionId, decimal buyPrice, decimal sellPrice, long stockQuantity)
+        {
+            BookEditionId = bookEditionId;
+            BuyPrice = buyPrice;
+            SellPrice = sellPrice;
+            StockQuantity = stockQuantity;
+        }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (BuyPrice < 0)
+                yield return new ValidationResult("Buy price cannot be negative.", new[] { nameof(BuyPrice) });
+            if (SellPrice < 0)
+                yield return new ValidationResult("Sell price cannot be negative.", new[] { nameof(SellPrice) });
+            if (StockQuantity < 0)
+                yield return new ValidationResult("Stock quantity cannot be negative.", new[] { nameof(StockQuantity) });
+        }
+    }
+
+}
---
+++ src/Bookstore.Core/Entities/Books/Books.cs
@@ -0,0 +1,91 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Bookstore.Entities.Books.BookConsts;
+
+namespace Bookstore.Entities.Books
+{
+    public static class BookConsts
+    {
+        public const int MaxTitleLength = 200;
+        public const int MaxAuthorLength = 50;
+        public const int MaxDescriptionLength = 5000;
+        public const int MinISBNLength = 13;
+        public enum Genre
+        {
+            Fiction,
+            NonFiction,
+            Mystery,
+            Thriller,
+            Romance,
+            Fantasy,
+            ScienceFiction,
+            Biography,
+            History,
+            Adventure,
+            Children,
+            SelfHelp,
+            Classic,
+            Travel,
+            Cooking,
+            Horror,
+            GraphicNovel
+        }
+        public enum Format
+        {
+            Hardcover,
+            Paperback
+        }
+    }
+
+    [Table("Books")]
+    public class Book : FullAuditedEntity<int>, IValidatableObject
+    {
+        [Required]
+        [StringLength(MaxTitleLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(MaxAuthorLength)]
+        public string Author { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [StringLength(MaxDescriptionLength)]
+        public string Description { get; set; }
+
+        // Relationships
+        public virtual ICollection<BookEdition> Editions { get; set; } = new List<BookEdition>();
+        public virtual ICollection<BookImage> Images { get; set; } = new List<BookImage>();
+
+        public Book() { }
+
+        public Book(string title, string author, Genre genre, string description)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Description = description;
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrWhiteSpace(Title))
+                yield return new ValidationResult("Title is required.", new[] { nameof(Title) });
+
+            if (string.IsNullOrWhiteSpace(Author))
+                yield return new ValidationResult("Author is required.", new[] { nameof(Author) });
+
+            if (!Enum.IsDefined(typeof(Genre), Genre))
+                yield return new ValidationResult("Genre is invalid.", new[] { nameof(Genre) });
+        }
+    }
+}
