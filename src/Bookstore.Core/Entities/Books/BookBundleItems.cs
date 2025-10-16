b98ff4da6e822bfa345ac48c0a94ce919438c49c
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2025-10-16 12:08:21 +0700

    Revert "blunder"
    This reverts commit 6ec923b145ca2482ef18dc0e7a17a5352768d52a.

---
+++ src/Bookstore.Core/Entities/Books/BookBundleItems.cs
@@ -0,0 +1,33 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Entities.Books
+{
+    [Table("BookBundleItems")]
+    public class BookBundleItem : FullAuditedEntity<int>
+    {
+        [ForeignKey(nameof(BookBundle))]
+        public int BundleId { get; set; }
+        public virtual BookBundle Bundle { get; set; }
+
+        [ForeignKey(nameof(Book))]
+        public int BookId { get; set; }
+        public virtual Book Book { get; set; }
+
+        public int Quantity { get; set; } = 1;
+
+        public BookBundleItem() { }
+
+        public BookBundleItem(int bundleId, int bookId, int quantity)
+        {
+            BundleId = bundleId;
+            BookId = bookId;
+            Quantity = quantity;
+        }
+    }
+}
